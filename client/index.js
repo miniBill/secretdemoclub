f22c2a964d7fec68466e44bcb462526e9de87d2a
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-05-21 02:57:33 +0200

    Add minimal service worker doing nothing

--- client/index.js
+++ client/index.js
@@ -20,7 +20,7 @@ for (var i = 0; i < localStorage.length; i++) {
  * @typedef {{ call: ( arg: T ) => void }} JsToElm<T>
  */
 
-/** @typedef {{ sendToLocalStorage: ElmToJs<{key : string, value: string}> }} Ports */
+/** @typedef {{ sendToLocalStorage: ElmToJs<{key : string, value: string}>, serviceWorkerRegistrationSuccess : JsToElm<{}> }} Ports */
 /** @typedef {{ ports: Ports }} ElmApp */
 
 /** @type {ElmApp} app */
@@ -33,3 +33,34 @@ const app = Elm.Main.init({
 app.ports.sendToLocalStorage.subscribe(({ key, value }) => {
     window.localStorage[key] = JSON.stringify(value);
 });
+
+if ("serviceWorker" in navigator) {
+    try {
+        const registration = await navigator.serviceWorker.register(
+            "/service-worker.js"
+        );
+
+        let serviceWorker;
+        if (registration.installing) {
+            serviceWorker = registration.installing;
+        } else if (registration.waiting) {
+            serviceWorker = registration.waiting;
+        } else if (registration.active) {
+            serviceWorker = registration.active;
+            app.ports.serviceWorkerRegistrationSuccess.call({});
+        }
+
+        if (serviceWorker) {
+            serviceWorker.addEventListener("statechange", () => {
+                if (serviceWorker.state == "activated") {
+                    app.ports.serviceWorkerRegistrationSuccess.call({});
+                }
+            });
+        }
+    } catch (error) {
+        // Sadge
+        console.error(error);
+    }
+}
+
+export {};
---
+++ client/service-worker.js
@@ -0,0 +1,27 @@
+self.addEventListener("install", function (/** @type {ExtendableEvent} */ e) {
+    console.log("Service worker installed:", e);
+});
+
+self.addEventListener("activate", function (/** @type {ExtendableEvent} */ e) {
+    console.log("Service worker activated:", e);
+});
+
+self.addEventListener("fetch", function (/** @type {FetchEvent} */ e) {
+    if (e.request.method !== "GET") return;
+
+    const url = new URL(e.request.url);
+    if (url.pathname !== "/download") {
+        console.log("Ignoring", url.pathname);
+        return;
+    }
+
+    debugger;
+
+    e.respondWith(
+        (async () => {
+            return fetch(e.request);
+        })()
+    );
+
+    debugger;
+});
